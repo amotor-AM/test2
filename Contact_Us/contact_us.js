73b40066b46d8f82ae2c7cca6dfa1a1838b7e949
amotor-AM <66324211+amotor-AM@users.noreply.github.com>
2020-09-23 16:00:35 -0700

    About us

---
+++ Contact_Us/contact_us.js
@@ -0,0 +1,9 @@
+// Menu Toggle
+
+function menuToggle() {
+  var nav = document.getElementById("menu-overlay");
+  nav.classList.toggle("active");
+
+  var nav = document.getElementById("toggleIcon");
+  nav.classList.toggle("active");
+}
